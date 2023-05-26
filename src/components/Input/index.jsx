d8811722bcba3f1a87694626858dcb14f34c07bd
murilonicemento <murilo_nascimento22@outlook.com>
2023-05-26 18:12:45 -0300

    feat: consumo api

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
-import { Form, MyInput, Cards } from "./styled";
+import {
+  Form,
+  MyInput,
+  LinkInput,
+  ErrorMessage,
+  Cards,
+  Button,
+} from "./styled";
 import axios from "axios";
 
 export function Input() {
   const [longURL, setLongURL] = useState("");
   const [shortURL, setShortURL] = useState([]);
+  const [inputEmpty, setInputEmpty] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
 
   async function handleSubmit(event) {
     event.preventDefault();
+    if (longURL == "") {
+      setInputEmpty(true);
+      return;
+    }
     try {
       const response = await axios.get(
         `https://api.shrtco.de/v2/shorten?url=${longURL}/very/long/link.html`
       );
-      let arr = [...response.data.result.full_short_link].join("");
-      setShortURL([arr]);
+      setShortURL([response.data.result.full_short_link]);
     } catch (error) {
       console.error(error);
     }
@@ -22,21 +34,32 @@ export function Input() {
   return (
     <Form onSubmit={handleSubmit}>
       <MyInput>
-        <input
-          onChange={(event) => setLongURL(event.target.value)}
-          value={longURL}
-          type="text"
-          placeholder="Shorten a link here..."
-        />
-        {/* <p>Please add a link</p> */}
-        <button>Shorten It!</button>
+        <div>
+          <LinkInput
+            onChange={(event) => setLongURL(event.target.value)}
+            value={longURL}
+            isEmpty={inputEmpty}
+            type="text"
+            placeholder="Shorten a link here..."
+          />
+          <ErrorMessage isEmpty={inputEmpty}>Please add a link</ErrorMessage>
+        </div>
+        <button type="submit">Shorten It!</button>
       </MyInput>
       {shortURL.map((url, index) => (
         <Cards key={index}>
           <p>{longURL}</p>
           <hr />
-          <p>{url}</p>
-          <button type="button">Copy</button>
+          <div>
+            <a href={url}>{url}</a>
+            <Button
+              type="button"
+              onClick={() => setIsCopied(true)}
+              copied={isCopied}
+            >
+              {isCopied ? "Copied!" : "Copy"}
+            </Button>
+          </div>
         </Cards>
       ))}
     </Form>
--- src/components/Input/styled.jsx
+++ src/components/Input/styled.jsx
@@ -7,12 +7,12 @@ export const Form = styled.form``;
 
 export const MyInput = styled.section`
   width: 90%;
-  height: 150px;
+  height: 180px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 10px;
+  gap: 20px;
   margin: 70px auto 0 auto;
   background-color: ${colors.statisticsCircle};
   background-image: url(${bgShortenMobile});
@@ -22,13 +22,8 @@ export const MyInput = styled.section`
   position: relative;
   z-index: 1;
 
-  input {
+  div {
     width: 90%;
-    height: 50px;
-    font-weight: 500;
-    padding: 20px;
-    border-radius: 4px;
-    color: ${colors.text};
   }
 
   button {
@@ -47,14 +42,42 @@ export const MyInput = styled.section`
     background-position: left;
     background-size: cover;
 
-    input {
-      width: 76%;
+    div {
+      width: 70%;
     }
 
     button {
       width: 14%;
     }
   }
+`;
+
+export const LinkInput = styled.input`
+  width: 100%;
+  height: 50px;
+  margin-bottom: 5px;
+  font-weight: 500;
+  padding: 20px;
+  border: ${(props) => (props.isEmpty ? "2px solid #F74E4E " : "none")};
+  border-radius: 4px;
+  color: ${colors.text};
+
+  ::placeholder {
+    color: ${(props) => (props.isEmpty ? "#F74E4E " : colors.text)};
+  }
+
+  @media (min-width: 800px) {
+    width: 100%;
+  }
+`;
+
+export const ErrorMessage = styled.p`
+  display: ${(props) => (props.isEmpty ? "flex" : "none")};
+  font-size: 13px;
+  font-weight: 400;
+  font-style: italic;
+  color: #f74e4e;
+  position: absolute;
 `;
 
 export const Cards = styled.div`
@@ -72,13 +95,28 @@ export const Cards = styled.div`
   p {
     margin-left: 20px;
     font-weight: 500;
+    color: ${colors.title};
+  }
 
-    &:nth-child(1) {
-      color: ${colors.title};
+  div {
+    width: 90%;
+    margin: auto;
+
+    a {
+      text-decoration: none;
+      font-weight: 500;
+      color: ${colors.button};
     }
 
-    &:nth-child(3) {
-      color: ${colors.button};
+    button {
+      width: 100%;
+      height: 50px;
+      background-color: ${colors.button};
+      margin-top: 15px;
+      font-weight: 600;
+      color: #fff;
+      border-radius: 4px;
+      cursor: pointer;
     }
   }
 
@@ -87,14 +125,30 @@ export const Cards = styled.div`
     opacity: 0.5;
   }
 
-  button {
-    width: 90%;
-    height: 50px;
-    background-color: ${colors.button};
-    margin: auto;
-    font-weight: 600;
-    color: #fff;
-    border-radius: 4px;
-    cursor: pointer;
+  @media (min-width: 800px) {
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    hr {
+      display: none;
+    }
+
+    div {
+      width: 30%;
+      display: flex;
+      align-items: center;
+      gap: 25px;
+      margin-right: 20px;
+
+      button {
+        margin: 0;
+      }
+    }
   }
+`;
+
+export const Button = styled.button`
+  background-color: ${(props) =>
+    props.copied ? colors.statisticsCircle : colors.button};
 `;
