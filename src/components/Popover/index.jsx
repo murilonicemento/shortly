c9ac05ab5aeb6cbf17f7635967f207635d38391a
murilonicemento <murilo_nascimento22@outlook.com>
2023-08-09 18:12:21 -0300

    ci: remoção do input e label

--- src/components/Popover/index.jsx
+++ src/components/Popover/index.jsx
@@ -131,30 +131,6 @@ const Fieldset = styled("fieldset", {
   alignItems: "center",
 });
 
-const Label = styled("label", {
-  fontSize: 13,
-  color: violet.violet11,
-  width: 75,
-});
-
-const Input = styled("input", {
-  all: "unset",
-  width: "100%",
-  display: "inline-flex",
-  alignItems: "center",
-  justifyContent: "center",
-  flex: "1",
-  borderRadius: 4,
-  padding: "0 10px",
-  fontSize: 13,
-  lineHeight: 1,
-  color: violet.violet11,
-  boxShadow: `0 0 0 1px ${violet.violet7}`,
-  height: 25,
-
-  "&:focus": { boxShadow: `0 0 0 2px ${violet.violet8}` },
-});
-
 const Text = styled("p", {
   margin: 0,
   color: mauve.mauve12,
